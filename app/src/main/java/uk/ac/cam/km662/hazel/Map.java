4a8edc10275d02efa135745316b0c4348abd6919
Snigdha Singhania <Snigdha Singhania>
2017-03-11 20:31:57 +0000

    Waiting for permissions

--- app/src/main/java/uk/ac/cam/km662/hazel/Map.java
+++ app/src/main/java/uk/ac/cam/km662/hazel/Map.java
@@ -7,6 +7,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphRequestAsyncTask;
+import com.facebook.GraphRequestBatch;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class Map extends AppCompatActivity {
 
     @Override
@@ -15,7 +31,7 @@ public class Map extends AppCompatActivity {
         setContentView(R.layout.activity_map);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,5 +41,7 @@ public class Map extends AppCompatActivity {
             }
         });
     }
+
+
 
 }
