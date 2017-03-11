902276d8c393776a462c0f9d8ac6a7742e58a6e4
Snigdha Singhania <Snigdha Singhania>
2017-03-11 16:21:46 +0000

    Event.java opens an external browser with the given event id

--- app/src/main/java/uk/ac/cam/km662/hazel/Event.java
+++ app/src/main/java/uk/ac/cam/km662/hazel/Event.java
@@ -1,11 +1,18 @@
 package uk.ac.cam.km662.hazel;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
 
 public class Event extends AppCompatActivity {
 
@@ -16,14 +23,36 @@ public class Event extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        Bundle extras = this.getIntent().getExtras();
+        String eventID = null;
+        if(extras != null){
+            eventID = extras.getString("eventID");
+        }
+        if(isValid(eventID))
+            viewEventDetails(eventID);
     }
 
+    private boolean isValid(String eventID){
+        return eventID != null;
+    }
+
+    protected void viewEventDetails(final String eventID){
+
+        String url = "https://www.facebook.com/events/"+eventID+"/";
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        startActivity(i);
+
+//        new GraphRequest(
+//                AccessToken.getCurrentAccessToken(),
+//                eventID,
+//                null,
+//                HttpMethod.GET,
+//                new GraphRequest.Callback() {
+//                    public void onCompleted(GraphResponse response) {
+//
+//                    }
+//                }
+//        ).executeAsync();
+    }
 }
