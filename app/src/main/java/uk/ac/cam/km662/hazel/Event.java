2374cc6f8ca0fcdc686dddff66b752e5f1f4f974
Snigdha Singhania <Snigdha Singhania>
2017-03-12 00:21:50 +0000

    Name of Event

--- app/src/main/java/uk/ac/cam/km662/hazel/Event.java
+++ app/src/main/java/uk/ac/cam/km662/hazel/Event.java
@@ -25,6 +25,10 @@ public class Event {
         return this.id;
     }
 
+    protected String getName(){
+        return this.name;
+    }
+
     protected double getLongitude(){
         return this.longitude;
     }
